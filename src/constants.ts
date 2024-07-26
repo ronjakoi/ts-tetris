7fe7125563849a81ee483bf4034f18bc38f51313
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2024-07-26 15:12:17 +0300

    parameterise level up gravity factor

--- src/constants.ts
+++ src/constants.ts
@@ -17,8 +17,10 @@ export const CANVAS_COLORS: { [key in Tile]: string } = {
 export const LOCK_DELAY_MS = 500;
 
 export const FRAMES_PER_SECOND = 60;
-
-export const STARTING_GRAVITY = 1 / 128; // 1 tile / n frames
+// 1 tile / n frames
+export const STARTING_GRAVITY = 1 / 128;
+// on level-up, multiply gravity by this:
+export const LEVEL_UP_GRAVITY_FACTOR = 2;
 export const SOFTDROP_GRAVITY = 1/8;
 // piece cannot move past the bottom in 1 frame
 export const MAX_GRAVITY = PLAYFIELD_HEIGHT;
